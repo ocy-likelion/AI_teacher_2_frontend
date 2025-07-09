930d462dd868ac8c07d7117eb099bc74a5bd9d8e
Eunji Park <eunjipark0129@gmail.com>
2025-07-09 10:36:32 +0900

    [AMT-19] 레이아웃 작업 (#4)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-type LayoutProps = {
-  children: React.ReactNode;
-};
-
-export default function Layout({ children }: LayoutProps) {
+export default function Layout() {
   return (
     <section className='bg-black flex justify-center min-h-[100dvh]'>
       <section className='bg-background-light dark:bg-gray7 safe-container w-full max-w-[var(--max-size-mobile)]'>
-        {children}
         <Outlet />
       </section>
     </section>
