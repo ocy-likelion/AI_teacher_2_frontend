33641f1805c378a238e9b0f450aa1db361fb5d4d
Taiho1998 <tgo12323@gmail.com>
2025-07-09 21:15:19 +0900

    feat : 회원 여부에 따라 인트로 메시지 변경

--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 
 export default function OnboardingPage() {
-  const [isUser, setIsUser] = useState<boolean>(false);
+  const [isUser, setIsUser] = useState<boolean>(true);
 
   var introMessage = isUser ? (
     <>
       <span className='text-primary'>일타</span>에 오신 걸 환영합니다!
     </>
   ) : (
-    `안녕하세요!
-          <br />
-          <span className='text-primary'>일타</span>는 처음 이용하시는 군요?`
+    <>
+      안녕하세요!
+      <br />
+      <span className='text-primary'>일타</span>는 처음 이용하시는 군요?{' '}
+    </>
   );
 
   return (
@@ -18,12 +20,7 @@ export default function OnboardingPage() {
       <div className='min-h-[115px] bg-black'>
         <img src='' alt='' className='aspect-square size-[49px]' />
         <h1 className='font-korean-title font-medium text-3xl'>
-          <span className='text-primary'>일타</span>에 오신 걸 환영합니다!
-        </h1>
-        <h1 className='font-korean-title font-medium text-3xl'>
-          안녕하세요!
-          <br />
-          <span className='text-primary'>일타</span>는 처음 이용하시는 군요?
+          {introMessage}
         </h1>
       </div>
     </div>
