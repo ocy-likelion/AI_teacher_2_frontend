d0012f4e104149f99ebf3acc8652cbf90d916ef6
Eunji <eunjipark0129@gmail.com>
2025-07-10 19:33:34 +0900

    feat : SearchSection 쿼리 파라미터 설정 로직 반영 및 스타일링

--- src/features/problems/list/SearchSection.tsx
+++ src/features/problems/list/SearchSection.tsx
@@ -1,12 +1,43 @@
 import { Search } from 'lucide-react';
+import React, { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 export default function SearchSection() {
+  const [input, setInput] = useState('');
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const currentQ = searchParams.get('q') ?? '';
+    if (input === currentQ) return;
+
+    const nextParams = new URLSearchParams(searchParams);
+    if (input) {
+      nextParams.set('q', input);
+    } else {
+      nextParams.delete('q');
+    }
+    console.log(nextParams);
+    navigate(`/history?${nextParams.toString()}`);
+  };
+
   return (
-    <section>
-      <input type='text' placeholder='검색어를 입력하세요' />
-      <button>
-        <Search />
+    <form
+      className='w-[280px] bg-white dark:bg-gray1 border border-gray3 dark:border-gray5 rounded-full px-5 py-3 flex justify-between items-center'
+      onSubmit={handleSubmit}
+    >
+      <input
+        id='search'
+        type='text'
+        placeholder='검색어를 입력하세요'
+        className='border-none outline-none placeholder:text-gray5 text-black body-sm'
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button className='cursor-pointer flex items-center justify-center'>
+        <Search className='text-gray5 active:scale-125 duration-300 ease-out' />
       </button>
-    </section>
+    </form>
   );
 }
