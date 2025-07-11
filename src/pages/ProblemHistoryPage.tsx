941f1df99371b001868f5d522d32a75c09a3c54f
Eunji <eunjipark0129@gmail.com>
2025-07-11 16:31:26 +0900

    feat : 검색 창 섹션, 탭 섹션 상단 고정

--- src/pages/ProblemHistoryPage.tsx
+++ src/pages/ProblemHistoryPage.tsx
@@ -5,7 +5,7 @@ import TabSection from '@/features/problems/list/TabSection';
 export default function ProblemHistoryPage() {
   return (
     <div className='w-full h-full flex flex-col items-center gap-3 px-7 py-3'>
-      <div className='bg-backgound-light dark:bg-gray7 z-10 w-full flex flex-col items-center gap-3'>
+      <div className='sticky top-[65px] bg-background-light dark:bg-gray7 z-10 w-full flex flex-col items-center gap-3 shadow-[var(--shadow-search)] dark:shadow-[var(--shadow-search-dark)]'>
         <SearchSection />
         <TabSection />
       </div>
