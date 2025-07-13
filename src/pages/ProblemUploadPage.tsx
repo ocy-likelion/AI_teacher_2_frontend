d7c3e63e76719c9146f34102475d7a02a0182248
Taiho1998 <tgo12323@gmail.com>
2025-07-14 03:07:06 +0900

    feat : 문제 업로드 페이지 마크업 및 스타일링 완료

--- src/pages/ProblemUploadPage.tsx
+++ src/pages/ProblemUploadPage.tsx
@@ -1,3 +1,35 @@
+import SubHeader from '@/components/layout/SubHeader';
+import { Button } from '@/components/ui/button';
+import { Info } from 'lucide-react';
+
 export default function ProblemUploadPage() {
-  return <></>;
+  return (
+    <>
+      <SubHeader type='close' title='문제 등록하기' />
+      <div className='mt-16 ml-[15px] flex flex-row'>
+        <Info size={16} className='text-primary mt-[4px] mr-[10px]' />
+        <p className='text- font-medium'>
+          문제는 하나씩만 등록할 수 있어요.
+          <br />
+          필요한 부분만 선택해주세요.
+        </p>
+      </div>
+      <section className='mt-[15px] w-full h-fit pb-10'>
+        <section className='flex justify-center w-full max-h-[40vh] aspect-square bg-black mb-10'>
+          <img src='/images/Sample-Image.svg' alt='사진 샘플' />
+        </section>
+        <div className='flex justify-center gap-6'>
+          <Button
+            className='w-[100px] dark:bg-gray7 bg-white text-primary border-1 border-primary hover:bg-primary/75 hover:border-primary/75 hover:text-white'
+            size={'lg'}
+          >
+            재업로드
+          </Button>
+          <Button className='w-[100px]' size={'lg'}>
+            확인
+          </Button>
+        </div>
+      </section>
+    </>
+  );
 }
