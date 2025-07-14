578547e840a1b84d353bac99121af2c8c4e80f1c
Taiho1998 <tgo12323@gmail.com>
2025-07-14 12:28:12 +0900

    feat : 이미지 업로드 시 페이지 이동 및 재업로드 버튼에 input 이벤트 추가.

--- src/features/users/components/ImageUploadSection.tsx
+++ src/features/users/components/ImageUploadSection.tsx
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import EditChildInfoForm from './EditChildInfo';
 import { Camera } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
+import ImageUpload from '@/features/problems/components/ImageUpload';
 
 export default function ImageUploadSection() {
   var divRef = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState<number>();
-
+  var inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  const ClickEventHandler = () => {
+    inputRef.current?.click();
+  };
   useEffect(() => {
     const resizeObserver: ResizeObserver = new ResizeObserver(() => {
       if (divRef.current) {
@@ -34,15 +39,16 @@ export default function ImageUploadSection() {
         </div>
         <h2>부모님!</h2>
       </div>
-      <Link
-        to='/problem/upload'
-        className='text-gray5 flex flex-col flex-grow items-center max-h-[40vh] max-w-full rounded-[24px] dark:text-gray2 inset-shadow-primary'
+      <div
+        onClick={ClickEventHandler}
+        className='text-gray5 flex flex-col flex-grow items-center max-h-[40vh] max-w-full rounded-[24px] dark:text-gray2 inset-shadow-primary cursor-pointer'
       >
+        {ImageUpload(inputRef, navigate, '/problem/upload')}
         <div ref={divRef} className='flex justify-center flex-grow flex-col'>
           <Camera width={120} height={height} />
           <span className='font-medium text-md'>문제를 등록해보세요</span>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
--- src/pages/ProblemUploadPage.tsx
+++ src/pages/ProblemUploadPage.tsx
@@ -1,11 +1,14 @@
 import SubHeader from '@/components/layout/SubHeader';
 import { Button } from '@/components/ui/button';
+import ImageUpload from '@/features/problems/components/ImageUpload';
 // import ImageCropper from '@/features/problems/components/ui/ImageCropper';
 import { Info } from 'lucide-react';
+import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function ProblemUploadPage() {
   const navigate = useNavigate();
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <>
       <SubHeader type='close' title='문제 등록하기' />
@@ -26,9 +29,11 @@ export default function ProblemUploadPage() {
           <Button
             className='w-[100px] dark:bg-gray7 bg-white text-primary border-1 border-primary hover:bg-primary/75 hover:border-primary/75 hover:text-white'
             size={'lg'}
+            onClick={() => inputRef.current?.click()}
           >
             재업로드
           </Button>
+          {ImageUpload(inputRef, navigate)}
           <Button
             onClick={() =>
               navigate('/problem/1', {
