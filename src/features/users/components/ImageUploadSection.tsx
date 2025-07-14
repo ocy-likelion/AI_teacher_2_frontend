25bea682498ef57674d52c6219d1aed31cc5400b
Taiho1998 <tgo12323@gmail.com>
2025-07-14 20:10:28 +0900

    feat : 문제 등록 버튼 클릭 시 카메라 촬영과 이미지 업로드 중 선택가능하게 함

--- src/features/users/components/ImageUploadSection.tsx
+++ src/features/users/components/ImageUploadSection.tsx
@@ -1,17 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 import EditChildInfoForm from './EditChildInfo';
-import { Camera } from 'lucide-react';
+import { Camera, Upload } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
 import ImageUpload from '@/features/problems/components/ImageUpload';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import ImageCapture from '@/features/problems/components/ImageCapture';
 
 export default function ImageUploadSection() {
-  var divRef = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState<number>();
-  var inputRef = useRef<HTMLInputElement>(null);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
   const navigate = useNavigate();
+
+  var divRef = useRef<HTMLDivElement>(null);
+  var uploadRef = useRef<HTMLInputElement>(null);
+  var cameraRef = useRef<HTMLInputElement>(null);
+
   const ClickEventHandler = () => {
-    inputRef.current?.click();
+    if (!dialogOpen) {
+      setDialogOpen(true);
+    }
   };
+  const CameraButtonEventHandler = () => {
+    setDialogOpen(false);
+    cameraRef.current?.click();
+  };
+  const UploadButtonEventHandler = () => {
+    setDialogOpen(false);
+    uploadRef.current?.click();
+  };
+
   useEffect(() => {
     const resizeObserver: ResizeObserver = new ResizeObserver(() => {
       if (divRef.current) {
@@ -43,12 +67,42 @@ export default function ImageUploadSection() {
         onClick={ClickEventHandler}
         className='text-gray5 flex flex-col flex-grow items-center max-h-[40vh] max-w-full rounded-[24px] dark:text-gray2 inset-shadow-primary cursor-pointer'
       >
-        {ImageUpload(inputRef, navigate, '/problem/upload')}
         <div ref={divRef} className='flex justify-center flex-grow flex-col'>
           <Camera width={120} height={height} />
           <span className='font-medium text-md'>문제를 등록해보세요</span>
         </div>
       </div>
+      <Dialog
+        open={dialogOpen}
+        onOpenChange={(isOpen) => {
+          setDialogOpen(isOpen);
+        }}
+      >
+        <DialogContent className='flex flex-row justify-center items-center py-20 gap-10'>
+          <DialogHeader className='hidden'>
+            <DialogTitle></DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <Button
+            onClick={CameraButtonEventHandler}
+            className='flex flex-col h-fit py-5'
+            size='lg'
+          >
+            <Camera className='!w-16 !h-16' />
+            <span className='text-xl'>카메라 촬영</span>
+          </Button>
+          <Button
+            onClick={UploadButtonEventHandler}
+            className='flex flex-col h-fit py-5'
+            size='lg'
+          >
+            <Upload className='!w-16 !h-16' />
+            <span className='text-xl'>파일 업로드</span>
+          </Button>
+        </DialogContent>
+      </Dialog>
+      {ImageUpload(uploadRef, navigate)}
+      {ImageCapture(cameraRef, navigate)}
     </div>
   );
 }
