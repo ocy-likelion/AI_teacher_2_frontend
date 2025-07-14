92fbc788d5ea84f0e54156aca7819017ccf2832c
Taiho1998 <tgo12323@gmail.com>
2025-07-14 09:14:49 +0900

    feat : 인트로 메시지 컴포넌트 생성

--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import LoginForm from '@/features/users/components/LoginForm';
 import ChildInfoNameInput from '@/features/users/components/ChildInfoNameInput';
 import ChildInfoGradeInput from '@/features/users/components/ChildInfoGradeInput';
+import OnBoardingIntro from '@/features/users/components/OnBoardingIntro';
 
 // const data = {
 //   id: 'gildongmom',
@@ -25,31 +26,9 @@ export default function OnboardingPage() {
     mode: 'onSubmit',
   });
 
-  useEffect(() => {}, [isUser]);
-  var introMessage = isUser ? (
-    <>
-      <span className='text-primary'>일타</span>에 오신 걸 환영합니다!
-    </>
-  ) : (
-    <>
-      안녕하세요!
-      <br />
-      <span className='text-primary'>일타</span>는 처음 이용하시는 군요?{' '}
-    </>
-  );
-
   return (
     <div className='w-full h-full mt-[50px]'>
-      <div className='min-h-[115px] px-6 mb-[10vh]'>
-        <img
-          src='/images/Logo_Img_noBg.svg'
-          alt=''
-          className='aspect-square size-[49px]'
-        />
-        <h1 className='font-korean-title font-medium text-3xl'>
-          {introMessage}
-        </h1>
-      </div>
+      {OnBoardingIntro(isUser)}
       {!!isUser && (
         <LoginForm
           handleSubmit={handleSubmit}
