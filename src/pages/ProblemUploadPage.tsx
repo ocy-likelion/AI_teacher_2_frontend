24072b6de46c8b646de909f787c6d695b7cfeab8
Taiho1998 <81617672+Taiho1998@users.noreply.github.com>
2025-07-15 11:09:49 +0900

    [AMT-36] 문제 등록 페이지 마크업 및 스타일링 (#14)
    * feat : 문제 업로드 페이지 마크업 및 스타일링 완료
    
    * feat : 이미지 크롭 용 라이브러리 설치 및 컴포넌트 작성
    
    * feat : 페이지 이동 후 upload 페이지 history에서 삭제
    
    * feat : 이미지 업로드 시 페이지 이동 및 재업로드 버튼에 input 이벤트 추가.
    
    * feat : 문제 등록 버튼 클릭 시 카메라 촬영과 이미지 업로드 중 선택가능하게 함
    
    * fix : 일부 함수 및 컴포넌트 정리
    
    * chore : 주석 정리
    
    * feat : footer의 문제 등록 버튼으로도 modal 띄울 수 있게 수정
    
    * feat : 이미지 업로드 시 페이지 이동 및 재업로드 버튼에 input 이벤트 추가.
    
    * feat : 문제 등록 버튼 클릭 시 카메라 촬영과 이미지 업로드 중 선택가능하게 함
    
    * fix : 일부 함수 및 컴포넌트 정리
    
    * fix : 수정 사항 반영
    
    ---------
    
    Co-authored-by: Eunji Park <eunjipark0129@gmail.com>

--- src/pages/ProblemUploadPage.tsx
+++ src/pages/ProblemUploadPage.tsx
@@ -1,3 +1,56 @@
+import SubHeader from '@/components/layout/SubHeader';
+import { Button } from '@/components/ui/button';
+import ImageInputModal from '@/features/problems/components/ImageInputModal';
+import { useImageModalStore } from '@/stores/imageModalStore';
+// import ImageCropper from '@/features/problems/components/ui/ImageCropper';
+import { Info } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+
 export default function ProblemUploadPage() {
-  return <></>;
+  const navigate = useNavigate();
+  const { isOpen, closeModal, openModal } = useImageModalStore();
+  return (
+    <>
+      <SubHeader type='close' title='문제 등록하기' />
+      <div className='mt-16 ml-[15px] flex flex-row'>
+        <Info size={16} className='text-primary mt-[4px] mr-[10px]' />
+        <p className='text- font-medium'>
+          문제는 하나씩만 등록할 수 있어요.
+          <br />
+          필요한 부분만 선택해주세요.
+        </p>
+      </div>
+      <section className='mt-[15px] w-full h-fit pb-10'>
+        <section className='flex justify-center w-full max-h-[40vh] aspect-square bg-black mb-10'>
+          <img src='/images/Sample-Image.svg' alt='사진 샘플' />
+          {/* <ImageCropper /> */}
+        </section>
+        <div className='flex justify-center gap-6'>
+          <Button
+            className='w-[100px] dark:bg-gray7 bg-white text-primary border-1 border-primary hover:bg-primary/75 hover:border-primary/75 hover:text-white'
+            size={'lg'}
+            onClick={openModal}
+          >
+            재업로드
+          </Button>
+          <ImageInputModal
+            dialogOpen={isOpen}
+            setDialogOpen={(value) => (value ? openModal() : closeModal())}
+          />
+          <Button
+            onClick={() =>
+              navigate('/problem/1', {
+                replace: true,
+                state: { from: 'upload' },
+              })
+            }
+            className='w-[100px]'
+            size={'lg'}
+          >
+            확인
+          </Button>
+        </div>
+      </section>
+    </>
+  );
 }
