13d7f8e72166a236d36a95f8e7eeb39b16d70ef5
Eunji <eunjipark0129@gmail.com>
2025-07-16 20:42:15 +0900

    feat : 홈페이지 키워드 모달 전역 상태 구조로 변경

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,23 @@
-import Tag from '@/components/ui/tag';
+import { Badge } from '@/components/ui/badge';
 import ImageUploadSection from '@/features/problems/components/ImageUploadSection';
 import EditChildInfoForm from '@/features/users/components/EditChildInfo';
+import { useModalStore } from '@/stores/modalStore';
+
+const conceptData = [
+  {
+    id: 1,
+    name: '원',
+    description: 'description',
+  },
+  {
+    id: 2,
+    name: '원의 넓이',
+    description: 'description',
+  },
+];
 
 export default function HomePage() {
+  const { openModal } = useModalStore();
   return (
     <>
       <div className='flex max-w-full min-h-[30vh] px-[25px] box-border mt-[10vh]'>
@@ -16,8 +31,20 @@ export default function HomePage() {
           자녀분이 어려워하는 <span className='text-primary'>주제</span>예요!
         </h2>
         <div className='flex mt-5 gap-5 flex-wrap'>
-          <Tag title='원' />
-          <Tag title='원의 넓이' />
+          {conceptData.map((concept) => (
+            <Badge
+              key={concept.id}
+              className='text-lg cursor-pointer'
+              onClick={() =>
+                openModal('CONCEPT', {
+                  title: concept.name,
+                  description: concept.description,
+                })
+              }
+            >
+              {concept.name}
+            </Badge>
+          ))}
         </div>
       </div>
     </>
