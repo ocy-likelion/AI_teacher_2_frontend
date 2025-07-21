bc7b3660df8e59cdda2a86478ea8079f56439c80
Eunji <eunjipark0129@gmail.com>
2025-07-21 17:41:19 +0900

    style : 설명 수정 및 레이아웃 조정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,26 +9,26 @@ const guideItems = [
   {
     id: 1,
     imageUrl: '/images/Icon_Camera.svg',
-    description: '문제가 잘 보이도록\n사진을 찍어주세요.',
+    description: '문제가 잘 보이게\n촬영해주세요',
   },
   {
     id: 2,
     imageUrl: '/images/Icon_Scissors.svg',
-    description: '풀이가 필요한 부분만\n잘라주세요.',
+    description: '한 문제만\n잘라주세요.',
   },
 ];
 
 export default function HomePage() {
   return (
-    <section className='w-full h-full pt-10 px-7 flex flex-col items-center justify-center'>
+    <section className='w-full h-full py-8 px-7 flex flex-col items-center gap-10'>
       <div className='relative w-full flex flex-col gap-2'>
-        <h2 className='title-md'>
+        <h2 className='break-keep title-sm'>
           수학 문제, 혼자 고민하지 마세요
           <br />
           <span className='text-primary'>사진</span> 한 장이면 충분해요
         </h2>
         <div className='flex gap-2'>
-          <p className='text-[14px] text-gray6 dark:text-gray2'>
+          <p className='break-keep text-[14px] text-gray6 dark:text-gray2'>
             AI가 수학 문제를 쉽게 설명해드려요.
           </p>
           <img
@@ -39,7 +39,7 @@ export default function HomePage() {
         </div>
       </div>
       <ImageUploadSection />
-      <section className='w-full flex items-center gap-5 pt-4'>
+      <section className='w-full flex items-center gap-5'>
         {guideItems.map((item) => (
           <GuideCard
             key={item.id}
@@ -54,7 +54,7 @@ export default function HomePage() {
 
 function GuideCard({ imageUrl, description }: GuideCardProps) {
   return (
-    <div className='w-full h-full flex flex-col items-center gap-1 px-3 py-4 rounded-[16px] shadow-[var(--shadow)] dark:shadow-[var(--shadow-dark)]'>
+    <div className='w-full flex flex-col items-center gap-1 px-3 py-4 rounded-[16px] shadow-[var(--shadow)] dark:shadow-[var(--shadow-dark)]'>
       <img src={imageUrl} alt='guide image' className='w-11 h-11' />
       <p className='text-[14px] text-center whitespace-pre-line'>
         {description}
