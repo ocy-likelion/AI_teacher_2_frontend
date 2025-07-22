04aa67f6aa6da1dd707020a18d2f84a852884bba
Eunji <eunjipark0129@gmail.com>
2025-07-22 20:00:54 +0900

    feat : 페이지 및 데이터 로딩 컴포넌트 작성, 아이콘 변경

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,3 +1,48 @@
-export default function Loading() {
-  return <div>로딩 중...</div>;
+type LoadingProps = {
+  description?: string;
+};
+
+export default function Loading({
+  description = '조금만 기다려주세요.',
+}: LoadingProps) {
+  return (
+    <div className='h-full flex flex-col justify-center items-center gap-3'>
+      <div className='w-full flex items-center justify-center gap-4'>
+        <object
+          data='/images/characters/orange_triangle.svg'
+          type='image/svg+xml'
+          className='w-12 h-12 animate-pop delay-0'
+        >
+          <img
+            src='/images/characters/orange_triangle.svg'
+            alt='orange_triangle'
+            className='w-12 h-12 animate-pop delay-0'
+          />
+        </object>
+        <object
+          data='/images/characters/green.svg'
+          type='image/svg+xml'
+          className='w-12 h-12 animate-pop delay-200'
+        >
+          <img
+            src='/images/characters/green.svg'
+            alt='초록'
+            className='w-12 h-12 animate-pop delay-200'
+          />
+        </object>
+        <object
+          data='/images/characters/orange_circle.svg'
+          type='image/svg+xml'
+          className='w-12 h-12 animate-pop delay-400'
+        >
+          <img
+            src='/images/characters/orange_circle.svg'
+            alt='주황'
+            className='w-12 h-12 animate-pop delay-400'
+          />
+        </object>
+      </div>
+      <p className='text-sm font-semibold'>{description}</p>
+    </div>
+  );
 }
