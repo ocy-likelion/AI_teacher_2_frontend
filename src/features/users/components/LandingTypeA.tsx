a47dbcba089a8b7550c4be179a900a335ef729b7
Eunji <eunjipark0129@gmail.com>
2025-07-22 16:09:41 +0900

    style : star 배경 z 인덱스 조정

--- src/features/users/components/LandingTypeA.tsx
+++ src/features/users/components/LandingTypeA.tsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 export default function LandingTypeA() {
   return (
     <section className='relative h-full w-full px-5'>
-      <section className='relative flex flex-col gap-3 z-100'>
-        <h1 className='title-md'>
+      <div className='pointer-events-none w-[380px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/5 z-10 star-pulse'>
+        <img src='/images/Star_Yellow.svg' alt='star' className='w-full z-0' />
+      </div>
+
+      <section className='relative flex flex-col gap-3 z-50'>
+        <h1 className='title-md z-50'>
           "엄마, 이거 어떻게 풀어?"
           <br />
           <span className='text-primary'>AI</span>가 부모님께
           <br />
           <span className='text-primary'>설명방법을</span> 알려드려요
         </h1>
-        <div className='text-gray5 dark:text-gray4 body-md z-100'>
+        <p className='text-gray5 dark:text-gray4 body-md z-50'>
           어려운 수학 문제, 이제 쉽게 설명해줄 수 있어요
-        </div>
+        </p>
       </section>
-
-      <div className='w-[380px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/5 z-0 star-pulse'>
-        <img src='/images/Star_Yellow.svg' alt='star' className='w-full z-0' />
-      </div>
 
       <section className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/5 w-80 h-96 z-10'>
         <div className='absolute top-13 left-0 rotate-green'>
