d17e9cf509d3ecbc896e3c455eb3673657ebc888
Taiho1998 <tgo12323@gmail.com>
2025-07-24 01:01:30 +0900

    refactor : 애니메이션 컴포넌트 리팩토링

--- src/features/users/components/OnboardingAnimation.tsx
+++ src/features/users/components/OnboardingAnimation.tsx
@@ -1,27 +1,27 @@
+const OBJECTELEMENTS = [
+  {
+    image: 'orange_triangle.svg',
+    style: 'left-[48%] top-[30vh] rotate-[10deg]',
+  },
+  { image: 'green.svg', style: 'left-[8%] top-[50vh] rotate-[-15deg]' },
+  { image: 'blue.svg', style: 'left-[53%] top-[65vh] rotate-[5deg]' },
+];
+
 export default function OnboardingAnimation() {
-  const durationA = `${Math.floor(Math.random() * 5000 + 1000)}ms`;
-  const durationB = `${Math.floor(Math.random() * 5000 + 1000)}ms`;
-  const durationC = `${Math.floor(Math.random() * 5000 + 1000)}ms`;
   return (
     <div>
-      <object
-        data='/images/characters/orange_triangle.svg'
-        type='image/svg+xml'
-        style={{ animationDuration: durationA }}
-        className='absolute w-[20vh] h-[20vh] left-[48%] top-[30vh] rotate-[10deg] drop-shadow-[1rem_-1rem_0_rgba(255,128,45,0.3)] dark:drop-shadow-[1rem_-1rem_0_var(--color-primary2)] animate-bounce'
-      />
-      <object
-        data='/images/characters/green.svg'
-        type='image/svg+xml'
-        style={{ animationDuration: durationB }}
-        className='absolute w-[20vh] h-[20vh] left-[8%] top-[50vh] rotate-[-15deg] drop-shadow-[1rem_-1rem_0_rgba(255,128,45,0.3)] dark:drop-shadow-[1rem_-1rem_0_var(--color-primary2)] animate-bounce'
-      />
-      <object
-        data='/images/characters/blue.svg'
-        type='image/svg+xml'
-        style={{ animationDuration: durationC }}
-        className='absolute w-[20vh] h-[20vh] left-[53%] top-[65vh] rotate-[5deg] drop-shadow-[1rem_-1rem_0_rgba(255,128,45,0.3)] dark:drop-shadow-[1rem_-1rem_0_var(--color-primary2)] animate-bounce'
-      />
+      {Array.from({ length: 3 }, (_, i) => i).map((value) => {
+        return (
+          <object
+            data={`/images/characters/${OBJECTELEMENTS[value].image}`}
+            type='image/svg+xml'
+            style={{
+              animationDuration: `${Math.floor(Math.random() * 5000 + 1000)}ms`,
+            }}
+            className={`absolute w-[20vh] h-[20vh] ${OBJECTELEMENTS[value].style} drop-shadow-[1rem_-1rem_0_rgba(255,128,45,0.3)] dark:drop-shadow-[1rem_-1rem_0_var(--color-primary2)] animate-bounce`}
+          />
+        );
+      })}
     </div>
   );
 }
