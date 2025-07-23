8ba6059df789d3fe8436a4ae043c9af3a593f75c
Taiho1998 <tgo12323@gmail.com>
2025-07-23 13:29:27 +0900

    style : progress 태그 커스텀 용으로 수정, 애니메이션 적용

--- src/features/users/components/OnBoardingIntro.tsx
+++ src/features/users/components/OnBoardingIntro.tsx
@@ -6,17 +6,17 @@ export default function OnBoardingIntro({ step }: OnBoardingIntroProp) {
   return (
     <div className='min-h-[115px] px-[31px] mt-[24px] mb-[10vh]'>
       <section className='h-fit'>
-        <progress
-          id='step'
-          className='w-full transition-transform duration-300'
-          value={step === 0 ? step + 1 : step}
-          max={3}
-        ></progress>
+        <div className='w-full h-2 bg-gray-200 rounded'>
+          <div
+            className='h-full bg-[#FF802D] transition-all duration-300 rounded'
+            style={{ width: `${(step / 3) * 100}%` }}
+          ></div>
+        </div>
         <label
           htmlFor='step'
           className='preahvihear-regular text-[13px] block text-right mt-[5px] mr-[2px]'
         >
-          {step === 0 ? step + 1 : step} / 3
+          {step} / 3
         </label>
       </section>
       {INTROMESSAGE[step]}
