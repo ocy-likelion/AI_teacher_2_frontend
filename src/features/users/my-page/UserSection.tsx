15e4b13f5ba703ea39f879eb90070a45b1f68c6c
Eunji <eunjipark0129@gmail.com>
2025-09-08 20:19:25 +0900

    feat : 마이페이지 반응형 적용

--- src/features/users/my-page/UserSection.tsx
+++ src/features/users/my-page/UserSection.tsx
@@ -5,9 +5,9 @@ export default function UserSection() {
     <section className='flex flex-col items-center gap-6'>
       <Profile />
       <div className='flex flex-col items-center gap-1'>
-        <p className='title-sm'>보호자 이름</p>
-        <p className='body-md'>username@example.com</p>
-        <p className='label text-gray5 dark:text-gray3 cursor-pointer active:font-semibold active:scale-105 duration-300 ease-in-out underline'>
+        <p className='title-sm md:title-md'>보호자 이름</p>
+        <p className='body-md md:title-sm'>username@example.com</p>
+        <p className='label md:body-sm text-gray5 dark:text-gray3 cursor-pointer active:font-semibold active:scale-105 duration-300 ease-in-out underline'>
           프로필 수정
         </p>
       </div>
